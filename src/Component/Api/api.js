3ed8c08463998df1951ed6dafd5d3bdb0898bce7
Vaibhav Zade <vaibhavz@incubxperts.com>
2024-06-16 09:49:46 +0530

    remove / from  end point

--- src/Component/Api/api.js
+++ src/Component/Api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_URL = "https://hotelmidasreegency.railway.internal";
-const API_URL = "https://hotelmidasreegency-production.up.railway.app/";
+const API_URL = "https://hotelmidasreegency-production.up.railway.app";
 
 export const PostApi = async (path, payload) => {
   try {
