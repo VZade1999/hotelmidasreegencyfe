5bb7094ccf766fb55667b64427d81b92e1ccdc96
Vaibhav Zade <vaibhavz@incubxperts.com>
2024-06-16 09:42:04 +0530

    change in endpoint

--- src/Component/Api/api.js
+++ src/Component/Api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "https://hotelmidasreegency.railway.internal";
-// const API_URL = "http://localhost:5000";
+// const API_URL = "https://hotelmidasreegency.railway.internal";
+const API_URL = "https://hotelmidasreegency-production.up.railway.app/";
 
 export const PostApi = async (path, payload) => {
   try {
